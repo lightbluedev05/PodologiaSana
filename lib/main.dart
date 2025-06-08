4ff7489a2404ba118e8ac79fb62a0e01e5d141e5
iammiguelsolis <miguelsoliscunza24@gmail.com>
2025-06-08 18:51:16 -0500

    no petes

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Podología Sana',
-      initialRoute: AppRoutes.dashboard_admin,
+      initialRoute: AppRoutes.login,
       onGenerateRoute: AppRoutes.generateRoute,
       theme: ThemeData(
         textTheme: GoogleFonts.robotoTextTheme(),
